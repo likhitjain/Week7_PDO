4840f6ec91ec73607166194fbe590da78e11bc91
Likhit V Jain <lvj5@afsconnect1.njit.edu>
2017-11-03 00:15:34 -0400

    second requirement

--- index.php
+++ index.php
@@ -3,14 +3,37 @@ $servername = "sql2.njit.edu";
 $username = "lvj5";
 $password = "mNoRgZ79";
 
+
 try {
+
     $conn = new PDO("mysql:host=$servername;dbname=lvj5", $username, $password);
+    
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully<br>"; 
+    echo "<hr>";
     }
-catch(PDOException $e)
+    catch(PDOException $e) 
     {
-    echo "Connection failed: " . $e->getMessage();
+    echo "Error: " . $e->getMessage();
     }
+   
+    $stmt = $conn->prepare("SELECT * FROM accounts where id<6"); 
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->fetchALL(PDO::FETCH_ASSOC); 
+    
+
+    echo '<table border="1"><tr>';
+    foreach ($result as $row) {
+        echo "<tr>";
+    foreach ($row as $column) {
+        echo "<td>$column</td>";
+   }
+    echo "</tr>";
+  }    
+    echo "</table>";
+
+  $conn = null;
 ?>
