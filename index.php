60699e1224f329c4d86ffc45cb8b9c48380b497b
Likhit V Jain <lvj5@afsconnect1.njit.edu>
2017-11-03 00:18:25 -0400

    update second requirement

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $password = "mNoRgZ79";
 try {
 
     $conn = new PDO("mysql:host=$servername;dbname=lvj5", $username, $password);
-    
+
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully<br>"; 
@@ -15,7 +15,7 @@ try {
     }
     catch(PDOException $e) 
     {
-    echo "Error: " . $e->getMessage();
+    echo "Connection failed: " . $e->getMessage();
     }
    
     $stmt = $conn->prepare("SELECT * FROM accounts where id<6"); 
